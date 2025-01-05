8dc1a8d42267ffe3388bef8765d3cb285fd4f4f8
PauloHenriqueBentoDosSantos <phbento@outlook.com.br>
2025-01-05 00:00:43 -0300

    initialize project with React, TypeScript, and Vite setup

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import pluginReactHooks from 'eslint-plugin-react-hooks'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    plugins: {
+      'react-hooks': pluginReactHooks,
+      prettier: prettierPlugin
+    },
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      'prettier/prettier': 'error'
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
+  },
+  eslintConfigPrettier
+]
